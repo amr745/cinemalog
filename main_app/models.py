afd70f08799753b2e0466bb03782e12c18261be5
Ashley Richard <ashleymrichard@gmail.com>
2022-07-23 21:15:24 -0700

    create, update, and delete class-based views work

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import date
+from django.urls import reverse
 
 GENRE = (
     ('D', 'Drama'),
@@ -15,7 +16,7 @@ GENRE = (
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     synopsis = models.TextField(max_length=250)
-    date = models.DateField('feeding date')
+    date = models.DateField('release date')
     rating = models.CharField(max_length=10)
     genre = models.CharField(
     max_length=1,
@@ -26,3 +27,6 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'movie_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Movie
 
 # class Movie:
@@ -15,6 +17,19 @@ from .models import Movie
 #   Movie('Top Gun: Maverick', ' May 27, 2022', 'After more than thirty years of service as one of the Navy"s top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancemen','PG-13', 'Action/Drama'),
 #   Movie('Nope', 'July 22, 2022', 'The residents of a lonely gulch in inland California bear witness to an uncanny and chilling discovery.', 'R', 'Horror/Mystery/Sci-Fi/Thriller'),
 # ]
+
+class MovieCreate(CreateView):
+  model = Movie
+  fields = '__all__'
+
+class MovieUpdate(UpdateView):
+  model = Movie
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['synopsis', 'date', 'rating', 'genre', 'url']
+
+class MovieDelete(DeleteView):
+  model = Movie
+  success_url = '/movies/'
 
 def home(request):
   return HttpResponse('<h1>Hello</h1>')
