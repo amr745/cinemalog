3baca62828d849f5cd7848a1d01274b2f3d810f4
Ashley Richard <ashleymrichard@gmail.com>
2022-07-23 20:02:43 -0700

    movies model works

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+GENRE = (
+    ('D', 'Drama'),
+    ('T', 'Thriller'),
+    ('R', 'Romance'),
+    ('A', 'Action'),
+    ('C', 'Comedy'),
+    ('H', 'Horror'),
+    ('S', 'Science Fiction'),
+    ('F', 'Fantacy'),
+)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    synopsis = models.TextField(max_length=250)
+    date = models.DateField('feeding date')
+    rating = models.CharField(max_length=10)
+    genre = models.CharField(
+    max_length=1,
+        choices=GENRE,
+        default=GENRE[0][0]
+    )
+    url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
-class Movie:
-  def __init__(self, title, date, description, rating, genre):
-    self.title = title
-    self.date = date
-    self.description = description
-    self.rating = rating
-    self.genre = genre
+# class Movie:
+#   def __init__(self, title, date, description, rating, genre):
+#     self.title = title
+#     self.date = date
+#     self.description = description
+#     self.rating = rating
+#     self.genre = genre
 
-movies = [
-  Movie('Thor: Love and Thunder', 'July 8, 2022', 'Thor enlists the help of Valkyrie, Korg and ex-girlfriend Jane Foster to fight Gorr the God Butcher, who intends to make the gods extinct.', 'PG-13', 'Action/Adventure/Comedy'),
-  Movie('Top Gun: Maverick', ' May 27, 2022', 'After more than thirty years of service as one of the Navy"s top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancemen','PG-13', 'Action/Drama' ),
-  Movie('Nope', 'July 22, 2022', 'The residents of a lonely gulch in inland California bear witness to an uncanny and chilling discovery.', 'R', 'Horror/Mystery/Sci-Fi/Thriller'),
-]
+# movies = [
+#   Movie('Thor: Love and Thunder', 'July 8, 2022', 'Thor enlists the help of Valkyrie, Korg and ex-girlfriend Jane Foster to fight Gorr the God Butcher, who intends to make the gods extinct.', 'PG-13', 'Action/Adventure/Comedy'),
+#   Movie('Top Gun: Maverick', ' May 27, 2022', 'After more than thirty years of service as one of the Navy"s top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancemen','PG-13', 'Action/Drama'),
+#   Movie('Nope', 'July 22, 2022', 'The residents of a lonely gulch in inland California bear witness to an uncanny and chilling discovery.', 'R', 'Horror/Mystery/Sci-Fi/Thriller'),
+# ]
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return HttpResponse('<h1>Hello</h1>')
 
 def about(request):
   return render(request, 'about.html')
 
 def movies_index(request):
+  movies = Movie.objects.all()
   return render(request, 'movies/index.html', { 'movies': movies })
+
+def movie_detail(request, movie_id):
+  movie = Movie.objects.get(id=movie_id)
+  return render(request, 'movies/detail.html', { 'movie': movie })
