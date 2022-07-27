89d6532c3f6d18215f33306380bbbf9433ed1670
Ashley Richard <ashleymrichard@gmail.com>
2022-07-26 19:03:09 -0700

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cinemalog.wsgi
--- cinemalog/settings.py
+++ cinemalog/settings.py
@@ -136,3 +136,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Configure Django App for Heroku
+import django_on_heroku
+django_on_heroku.settings(locals())
