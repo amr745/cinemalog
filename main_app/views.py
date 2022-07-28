9e1eb9c2794dbb5b04e5e3f0427a5f8e8bbc8ec6
Ashley Richard <ashleymrichard@gmail.com>
2022-07-27 19:37:51 -0700

    streams url works

--- main_app/views.py
+++ main_app/views.py
@@ -59,21 +59,6 @@ def add_view(request, movie_id):
     new_view.save()
   return redirect('detail', movie_id=movie_id)
 
-# @login_required
-# def add_photo(request, movie_id):
-#     photo_file = request.FILES.get('photo-file', None)
-#     if photo_file:
-#         s3 = boto3.client('s3')
-#         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-#         try:
-#             s3.upload_fileobj(photo_file, BUCKET, key)
-#             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-#             photo = Movie(url=url, movie_id=movie_id)
-#             photo.save()
-#         except:
-#             print('An error occurred uploading file to S3')
-#     return redirect('detail', movie_id=movie_id)
-
 class StreamList(LoginRequiredMixin, ListView):
   model = Stream
 
@@ -90,7 +75,7 @@ class StreamUpdate(LoginRequiredMixin, UpdateView):
 
 class StreamDelete(LoginRequiredMixin, DeleteView):
   model = Stream
-  success_url = '/stream/'
+  success_url = '/streams/'
 
 @login_required
 def assoc_stream(request, movie_id, stream_id):
